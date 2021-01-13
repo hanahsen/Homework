5d29faac39dd2560316736f2a05fea18261a8395
hanahsen <77193148+hanahsen@users.noreply.github.com>
2021-01-13 19:28:04 +0300

    upload homework

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Hana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Enter Your Age----");
+            string age = Console.In.ReadLine();
+            int int_age = int.Parse(age);
+            int yearOfBirh = 2021 - int_age;
+            Console.WriteLine("----Year Of Birth----");
+            Console.WriteLine(yearOfBirh);
+
+        }
+    }
+}
